a0082a18bfd6206e3ee886e3501aceb937c1c321
jsewai <junyasewai@gmail.com>
2021-03-22 19:21:34 -0400

    test api call and parse it by json and loop for each keys and values

--- stockviewer/quotes/views.py
+++ stockviewer/quotes/views.py
@@ -2,7 +2,19 @@ from django.shortcuts import render
 
 
 def home(request):
-  return render(request, 'home.html', {})
+# pk_bdcd2f6f2976438383fade06ae364594
+  import requests
+  import json
+
+  api_request = requests.get("https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_bdcd2f6f2976438383fade06ae364594")
+
+  try:
+    api = json.loads(api_request.content)
+  except Exception as e:
+    api = "Error..."
+
+
+  return render(request, 'home.html', {'api': api})
 
 def about(request):
   return render(request, 'about.html', {})
